670cbd99d3e64fe1e212f486c56e567ad95ee05c
JonnathanEspinoza <jonnathan.espinoza684j@gmail.com>
2020-11-06 18:43:45 -0500

    configuracion de react, webpack, babel

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/app/index.js',
+    output: {
+        path: __dirname + '/src/public',
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        rules: [
+            {
+                use: ['babel-loader'],
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/
+            }
+        ]
+    }
+};
